f4d020ba600bdf0838122b654f8886e95e3ae081
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2021-06-05 18:34:06 +0200

    Update Contact page

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -8,7 +8,7 @@ get_header();
 ?>
 
 <main data-barba="container" data-barba-namespace="portfolio">
-  <div class="page-title"><?= the_title() ?></div>
+    <div class="page-title"><?= the_title() ?></div>
 
     <section class="contact">
         <?php
@@ -24,6 +24,73 @@ get_header();
 
             <p class="contact__content-text"><?php the_field('contact_project_text'); ?></p>
         </div>
+
+    </section>
+
+    <section class="details">
+        <h2 class="details__title"><?php the_field('contact_details_title'); ?></h2>
+
+        <?php if( have_rows('contact_details_1') ): ?>
+            <?php while( have_rows('contact_details_1') ): the_row();
+
+                // Get sub field values.
+
+                $officeName = get_sub_field('contact_details_1_name');
+                $officeMail = get_sub_field('contact_details_1_mail');
+                $officeAddress = get_sub_field('contact_details_1_address');
+                $officePhone = get_sub_field('contact_details_1_phone');
+                $officeMapLink = get_sub_field('contact_details_1_link');
+                $officeMapLinkText = get_sub_field('contact_details_1_link_text');
+
+                ?>
+
+                <div class="details__office">
+                    <div class="office">
+                        <p class="office__name"><?php echo $officeName ?></p>
+                        <ul class="office__details">
+                            <li class="office__details-mail">Mail: <?php echo $officeMail ?></li>
+                            <li class="office__details-address">Address: <?php echo $officeAddress ?></li>
+                            <li class="office__details-phone">Phone: <?php echo $officePhone ?></li>
+                        </ul>
+                    </div>
+                    <a class="office__map-link" target="_blank" href="<?php echo esc_url( $officeMapLink['url'] ); ?>"><?php echo $officeMapLinkText; ?></a>
+                </div>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php if( have_rows('contact_details_2') ): ?>
+            <?php while( have_rows('contact_details_2') ): the_row();
+
+                // Get sub field values.
+
+                $officeName = get_sub_field('contact_details_2_name');
+                $officeMail = get_sub_field('contact_details_2_mail');
+                $officeAddress = get_sub_field('contact_details_2_address');
+                $officePhone = get_sub_field('contact_details_2_phone');
+                $officeMapLink = get_sub_field('contact_details_2_link');
+                $officeMapLinkText = get_sub_field('contact_details_2_link_text');
+
+                ?>
+
+                <div class="details__office">
+                    <div class="office">
+                        <p class="office__name"><?php echo $officeName ?></p>
+                        <ul class="office__details">
+                            <li class="office__details-mail">Mail: <?php echo $officeMail ?></li>
+                            <li class="office__details-address">Address: <?php echo $officeAddress ?></li>
+                            <li class="office__details-phone">Phone: <?php echo $officePhone ?></li>
+                        </ul>
+                    </div>
+                    <a class="office__map-link" target="_blank" href="<?php echo esc_url( $officeMapLink['url'] ); ?>"><?php echo $officeMapLinkText; ?></a>
+                </div>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+    </section>
+
+    <section class="form">
 
     </section>
 </main>
